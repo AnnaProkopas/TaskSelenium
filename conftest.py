5a53ca5a238a169997de2fff97e223fd4fc9fe22
user <anna_prokopas@mail.ru>
2019-09-19 11:44:11 +1000

    add cross-browser compatibility

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 example_languages = ['ru', 'fr', 'es']
 
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="ru",
                      help="Choose language: for example, ru")
 
@@ -12,9 +14,17 @@ def pytest_addoption(parser):
 def browser(request):
     user_language = request.config.getoption("language") 
     user_language = 'ru' if not (str(user_language) in example_languages) else user_language
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\nquit browser..")
     browser.quit()
